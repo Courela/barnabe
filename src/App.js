a4b6c7101f48abf6ee6f941ddfc31854a909eb1f
nunocourela <nuno.courela@nmbrs.nl>
2018-04-04 23:09:14 +0100

    Init

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { Component, Fragment } from "react";
+import { Link } from "react-router-dom";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import "./App.css";
+import Routes from "./Routes";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      isAuthenticated: false
+    };
+  }
+  
+  userHasAuthenticated = authenticated => {
+    this.setState({ isAuthenticated: authenticated });
+  }
+
+  handleLogout = event => {
+    this.userHasAuthenticated(false);
+  }
+
+  render() {
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated
+    };
+
+    return (
+      <div className="App container">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">Taça Barnabé</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <LinkContainer to="/about">
+                <NavItem>Sobre</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/torneio">
+                <NavItem>Torneio</NavItem>
+              </LinkContainer>
+              {this.state.isAuthenticated
+                ? <NavItem onClick={this.handleLogout}>Logout</NavItem>
+                : <Fragment>
+                    <LinkContainer to="/login">
+                      <NavItem>Login</NavItem>
+                    </LinkContainer>
+                  </Fragment>
+              }
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Routes childProps={childProps} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/NotFound.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./NotFound.css";
+
+export default () =>
+  <div className="NotFound">
+    <h3>Página não encontrada!</h3>
+  </div>;

bd71c5ce8defa448ed48ce0293e89c8aaf446cf0
nunocourela <nuno.courela@nmbrs.nl>
2018-04-06 23:48:40 +0100

    ReactTable, players mock

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,16 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
+import Torneio from "./containers/Torneio";
+import Player from "./containers/Player";
 import AppliedRoute from "./components/AppliedRoute";
 
 export default ({ childProps }) =>
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
+    <AppliedRoute path="/torneio" exact component={Torneio} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
+    <AppliedRoute path="/player/:id" component={Player} />
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
   </Switch>;

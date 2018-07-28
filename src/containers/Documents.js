47337b09f9216df694d98e38d5d9268c022f5570
nunocourela <nuno.courela@nmbrs.nl>
2018-07-28 11:53:57 +0100

    Documents shared on Drive

--- src/containers/Documents.js
+++ src/containers/Documents.js
@@ -4,14 +4,14 @@ import { Table } from 'react-bootstrap';
 export default class Documents extends Component {
     render() {
         const data = [
-            { name: 'Ficha_Inscricao_Jogador.pdf', type: 'Ficha Individual de Jogador', download: '/doc/Ficha_Inscricao_Jogador.pdf' },
-            { name: 'Regulamento_Desportivo_2018.pdf', type: 'Regulamento Desportivo', download: '/doc/Regulamento_Desportivo_2018.pdf' }
+            { name: 'Ficha_Inscricao_Jogador.pdf', type: 'Ficha Individual de Jogador', download: 'https://drive.google.com/open?id=1JvEZAj804aTE7aO2ydLCumJvts_bQh3D' },
+            { name: 'Regulamento_Desportivo_2018.pdf', type: 'Regulamento Desportivo', download: 'https://drive.google.com/open?id=1HdbvEZwIiopL1azo8i7w75XAQBbyiFAI' }
         ];
 
         const rows = data.map((row, idx) => <tr key={idx}>
             <td>{row.name}</td>
             <td>{row.type}</td>
-            <td><a href={row.download}>Download</a></td>
+            <td><a href={row.download} target="_blank" rel="noopener noreferrer">Download</a></td>
         </tr>);
 
         const table = <Table striped bordered condensed hover style={{ width: '70%'}}>
--- src/utils/validations.js
+++ src/utils/validations.js
@@ -4,11 +4,12 @@ function validateNotEmpty(str) {
 };
 
 function isValidEmail(email) {
-    return (email === '' || email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i));
+    return (!email || email === '' || email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i));
 }
 
 function isValidPhone(phoneNr) {
-    return (phoneNr === '' || phoneNr.replace(/ /g, '').match(/^(\+351|00351|351)?(9[1236][0-9]{7}|2[1-9][0-9]{7})$/));
+    console.log('PhoneNr: ', phoneNr);
+    return (!phoneNr || phoneNr === '' || phoneNr.replace(/ /g, '').match(/^(\+351|00351|351)?(9[1236][0-9]{7}|2[1-9][0-9]{7})$/));
 }
 
 export {

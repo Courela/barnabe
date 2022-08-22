af2b7903c469b57668b7471b10b737e993695aa8
Courela <courela@gmail.com>
2022-08-22 23:53:31 +0100

    Update Ficha_Inscricao_Jogador.pdf link

--- src/containers/Documents.js
+++ src/containers/Documents.js
@@ -4,7 +4,7 @@ import { Table } from 'react-bootstrap';
 export default class Documents extends Component {
     render() {
         const data = [
-            { name: 'Ficha_Inscricao_Jogador.pdf', type: 'Ficha Individual de Jogador', download: 'https://drive.google.com/open?id=1B-eFuHdWttK77IaNaYiKUW9Wk8Nb-a_f' },
+            { name: 'Ficha_Inscricao_Jogador.pdf', type: 'Ficha Individual de Jogador', download: 'https://drive.google.com/file/d/1WgNrlMmyZnD_-TAFRH40G1TZCiKr7bYj/view?usp=sharing' },
             { name: 'Regulamento_Desportivo_2022.pdf', type: 'Regulamento Desportivo 2022', download: 'https://drive.google.com/file/d/1Uj-wNu0Gv4q9FGyd8EEhgQ2N6VpBUYC4/view?usp=sharing' },
             { name: 'Regulamento_Organizacao.pdf', type: 'Regulamento da Organização', download: 'https://drive.google.com/open?id=0B6mGDIzH6fPwOEdwTnREMmVFbUttSjlvdlNDamNVOWUzSHBR' }
             // { name: 'MapaPavilhao.pdf', type: 'Mapa de ocupação de pavilhão', download: 'https://drive.google.com/file/d/1giu3mqrjgrED9WaaKovQcVmxVrpjq9Kx/view?usp=sharing' }

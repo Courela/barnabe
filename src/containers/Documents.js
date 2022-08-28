6b8364a14f79f7aa3603374aa8408e54b3b2bee2
Courela <courela@gmail.com>
2022-08-28 18:05:39 +0100

    Update doc link

--- src/containers/Documents.js
+++ src/containers/Documents.js
@@ -6,7 +6,7 @@ export default class Documents extends Component {
         const data = [
             { name: 'Ficha_Inscricao_Jogador.pdf', type: 'Ficha Individual de Jogador', download: 'https://drive.google.com/file/d/1WgNrlMmyZnD_-TAFRH40G1TZCiKr7bYj/view?usp=sharing' },
             { name: 'Regulamento_Desportivo_2022.pdf', type: 'Regulamento Desportivo 2022', download: 'https://drive.google.com/file/d/1Uj-wNu0Gv4q9FGyd8EEhgQ2N6VpBUYC4/view?usp=sharing' },
-            { name: 'Regulamento_Organizacao.pdf', type: 'Regulamento da Organização', download: 'https://drive.google.com/open?id=0B6mGDIzH6fPwOEdwTnREMmVFbUttSjlvdlNDamNVOWUzSHBR' }
+            { name: 'Regulamento_Organizacao.pdf', type: 'Regulamento da Organização', download: 'https://drive.google.com/file/d/0B6mGDIzH6fPwOEdwTnREMmVFbUttSjlvdlNDamNVOWUzSHBR/view?usp=sharing&resourcekey=0-D_JJwmynKutiIrKPQieC6A' }
             // { name: 'MapaPavilhao.pdf', type: 'Mapa de ocupação de pavilhão', download: 'https://drive.google.com/file/d/1giu3mqrjgrED9WaaKovQcVmxVrpjq9Kx/view?usp=sharing' }
         ];
 
